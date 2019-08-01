a47b427522c390d247cb98d45263094effa67d8e
trewanek <trewanek@gmail.com>
2019-08-02 00:11:04 +0900

    Fix. change `getFullName`(private) to `GetFullName`(public)

--- domain/user.go
+++ domain/user.go
@@ -29,7 +29,7 @@ const (
 	nameSeparator = " "
 )
 
-func (u *User) getFullName() string {
+func (u *User) GetFullName() string {
 	return u.FirstName + nameSeparator + u.LastName
 }
 
--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/martian/log"
 	"github.com/trewanek/LayeredArchitectureWithGolang/application"
 )
 
 func main() {
 	appUser := application.NewUser()
-	fmt.Println(appUser.GetUserByID("documentID"))
+	user, err := appUser.GetUserByID("documentID")
+	if err != nil {
+		log.Errorf(err.Error())
+		return
+	}
+	fmt.Println(user.GetFullName())
 }
